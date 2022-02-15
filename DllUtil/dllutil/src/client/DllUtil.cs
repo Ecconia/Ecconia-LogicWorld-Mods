8e5c22c58c017586bbc7c564f396cb5d8cfa3941
Ecconia <public-commit@ecconia.com>
2022-02-15 15:59:19 +0100

    Fix formatting in external mods
    Formats the mods by FalsePattern a bit.
    - Use tabs
    - Add newline at end of file
    - Generally improve spacing

--- DllUtil/dllutil/src/client/DllUtil.cs
+++ DllUtil/dllutil/src/client/DllUtil.cs
@@ -8,42 +8,56 @@ using LogicLog;
 
 using UnityEngine;
 
-namespace DllUtil {
-    public class Loader: ClientMod {
-        private static Loader instance = null;
-        private string root = null;
-        protected override void Initialize() {
-            instance = this;
-            Logger.Info("Initializing DllUtil...");
-            root = string.Concat(Application.temporaryCachePath, "/dllcache/");
-            Logger.Trace("Clearing cache directory...");
-            try {
-                Directory.Delete(root, true);
-            } catch (Exception ignored){}
-            Directory.CreateDirectory(root);
-        }
+namespace DllUtil
+{
+	public class Loader: ClientMod
+	{
+		private static Loader instance = null;
 		
-        internal Assembly LoadInternal(ModFile file) {
-            try {
-                Logger.Info(string.Concat("Loading ", file.FileName));
-                var path = string.Concat(root, file.FileName, System.Guid.NewGuid().ToString(), file.Extension);
-                Logger.Trace(string.Concat("Exporting ", file.FileName, " to ", path));
-                var fileStream = File.Create(path);
-                var modStream = file.OpenRead();
-                modStream.CopyTo(fileStream);
-                fileStream.Close();
-                Logger.Trace(string.Concat("Successfully exported ", file.FileName, "!"));
-                var assembly = Assembly.LoadFrom(path);
-                Logger.Info(string.Concat("Successfully loaded ", file.FileName));
-                return assembly;
-            } catch (Exception e) {
-                Logger.Fatal(string.Concat("Failed to load ", file.FileName, "!\n", e.StackTrace));
-                return null;
-            }
-        } 
+		private string root = null;
 		
-        public static Assembly Load(ModFile file) {
-            return instance.LoadInternal(file);
-        }
-    }
+		protected override void Initialize()
+		{
+			instance = this;
+			Logger.Info("Initializing DllUtil...");
+			root = string.Concat(Application.temporaryCachePath, "/dllcache/");
+			Logger.Trace("Clearing cache directory...");
+			try
+			{
+				Directory.Delete(root, true);
+			}
+			catch (Exception ignored)
+			{
+			}
+			Directory.CreateDirectory(root);
+		}
+		
+		internal Assembly LoadInternal(ModFile file)
+		{
+			try
+			{
+				Logger.Info(string.Concat("Loading ", file.FileName));
+				var path = string.Concat(root, file.FileName, System.Guid.NewGuid().ToString(), file.Extension);
+				Logger.Trace(string.Concat("Exporting ", file.FileName, " to ", path));
+				var fileStream = File.Create(path);
+				var modStream = file.OpenRead();
+				modStream.CopyTo(fileStream);
+				fileStream.Close();
+				Logger.Trace(string.Concat("Successfully exported ", file.FileName, "!"));
+				var assembly = Assembly.LoadFrom(path);
+				Logger.Info(string.Concat("Successfully loaded ", file.FileName));
+				return assembly;
+			}
+			catch (Exception e)
+			{
+				Logger.Fatal(string.Concat("Failed to load ", file.FileName, "!\n", e.StackTrace));
+				return null;
+			}
+		} 
+		
+		public static Assembly Load(ModFile file)
+		{
+			return instance.LoadInternal(file);
+		}
+	}
 }
--- LWHarmony/lwharmony/src/client/LWHarmony.cs
+++ LWHarmony/lwharmony/src/client/LWHarmony.cs
@@ -1,15 +1,22 @@
+using System.Reflection;
+
 using LogicAPI.Client;
 using LogicLog;
-using System.Reflection;
-namespace LWHarmony {
-    public class Injector: ClientMod {
-        protected override void Initialize() {
-            foreach (var file in Files.EnumerateFiles()) {
-                if (file.Path.EndsWith(".dll")) {
-                    Logger.Info(string.Concat("Loading ", file.FileName));
-                    DllUtil.Loader.Load(file);
-                }
-            }
-        }
-    }
+
+namespace LWHarmony
+{
+	public class Injector: ClientMod
+	{
+		protected override void Initialize()
+		{
+			foreach (var file in Files.EnumerateFiles())
+			{
+				if (file.Path.EndsWith(".dll"))
+				{
+					Logger.Info(string.Concat("Loading ", file.FileName));
+					DllUtil.Loader.Load(file);
+				}
+			}
+		}
+	}
 }
