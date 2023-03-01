407e4e050b9bf245e967e9e5655c68f310eb0ce6
Akasus <akasus.nevelas@gmail.com>
2023-03-01 14:30:55 +0100

    Added preloading Procedure

--- AssemblyLoader/AssemblyLoader/src/server/ModClass.cs
+++ AssemblyLoader/AssemblyLoader/src/server/ModClass.cs
@@ -1,8 +1,29 @@
+using System;
+using System.Linq;
+using AssemblyLoader.Shared;
 using LogicAPI.Server;
 
 namespace AssemblyLoader.server
 {
-	public class ModClass : ServerMod
-	{
-	}
+    public class ModClass : ServerMod
+    {
+        protected override void Initialize()
+        {
+            Logger.Info("AssemblyLoader Initialize...");
+
+            foreach (var file in ModHelper.GetDependentModFiles(Files, Manifest.ID)
+                         .Where(e => e.Path.Contains("server") || e.Path.Contains("shared")))
+            {
+                try
+                {
+                    DLLLoader.loadAssemblyFromModFile(file);
+                    Logger.Info($"Load Assembly {file.FileName}");
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e.ToString());
+                }
+            }
+        }
+    }
 }
---
+++ AssemblyLoader/AssemblyLoader/src/shared/ModHelper.cs
@@ -0,0 +1,34 @@
+﻿// File: ModHelper.cs
+// Project: AssemblyLoader
+// Author: Akasus (akasus.nevelas@gmail.com)
+// Description:
+
+using LogicAPI.Modding;
+using LogicAPI;
+using LogicWorld.SharedCode.Modding;
+using SUCC;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace AssemblyLoader.Shared;
+
+public class ModHelper
+{
+    public static IEnumerable<ModFile> GetDependentModFiles(IModFiles files, string modID)
+    {
+        foreach (var dir in Directory.EnumerateDirectories(Path.Combine(files.Path, ".."), "*"))
+        {
+            var manifest = new DataFile(Path.Combine(dir, "manifest.succ")).GetAsObject<ModManifest>();
+            if (!manifest.Dependencies.Contains(modID)) continue;
+
+            var modFiles = new FolderModFiles(dir);
+
+            foreach (var file in modFiles.EnumerateFiles("*.dll"))
+            {
+                yield return file;
+            }
+        }
+    }
+}
