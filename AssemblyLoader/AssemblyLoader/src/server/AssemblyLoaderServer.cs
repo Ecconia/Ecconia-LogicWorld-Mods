9106474c1e10c5f03c15eeff1bb2f3d38cb97116
Ecconia <public-commit@ecconia.com>
2023-03-02 15:05:29 +0100

    AL: Load all server assemblies on launch
    Prevents crash on compiling of some mods.

--- AssemblyLoader/AssemblyLoader/src/server/AssemblyLoaderServer.cs
+++ AssemblyLoader/AssemblyLoader/src/server/AssemblyLoaderServer.cs
@@ -15,7 +15,17 @@ namespace AssemblyLoader.Server
 		{
 			//Set the logger, to be used when loading assemblies:
 			AssemblyLoader.Shared.AssemblyLoader.logger = Logger;
-			
+
+			//Since 0.91 the server does no longer load all DLLs it comes with.
+			// Some mods however need some of these DLLs to compile, the compilation will fail.
+			// As solution one can ensure that every delivered DLL is loaded.
+			//Originally this issue occured first for cheese's util mods.
+			// Which CheeseLoader was written for.
+			// This mod will have the same functionality, as both mods can coexist.
+			// It is one less required dependency for users of Eccs mods.
+			//Part of the reasons for the reimplementation is that currently CheeseLoader does not support ServerOnlyMods.
+			ServerAssemblyLoader.loadAllServerAssemblies(Logger);
+
 			//The following step is rather expensive, as it will find mods again, which LW-Server already did.
 			// But LW-Server did not save the result anywhere and is straight up processing it.
 			// The safest least hacky way of obtaining the list, is to simply scan for mods again.
