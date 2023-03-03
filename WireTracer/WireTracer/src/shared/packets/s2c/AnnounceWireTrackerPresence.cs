7c23d6c0502bcb23ad648963fdf9ba277849bd7d
Ecconia <public-commit@ecconia.com>
2023-03-03 23:28:52 +0100

    WT: Fix crash on Windows when empty packet is set
    By just simply adding a single pointless obsolete byte.

--- WireTracer/WireTracer/src/shared/packets/s2c/AnnounceWireTrackerPresence.cs
+++ WireTracer/WireTracer/src/shared/packets/s2c/AnnounceWireTrackerPresence.cs
@@ -6,5 +6,7 @@ namespace WireTracer.Shared.Packets.S2C
 	[MessagePackObject]
 	public class AnnounceWireTrackerPresence : Packet
 	{
+		[Key(0)]
+		public byte unused_but_required_as_when_missing_will_crash_logic_world_on_windows = 0;
 	}
 }
