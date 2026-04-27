89956cf6b3848dbb02969f0a008b5d6ae0226930
Ecconia <public-commit@ecconia.com>
2026-04-27 14:50:56 +0200

    EGB: Add JetBrains usage annotation to [AssignMe]
    IDEs would should fields with [AssignMe] to not be initialized. This is wrong, as it gets initialized using reflection.
    JetBrains added a way to tell IDEs this, using their annotation gets rid of the warnings.

--- EccsGuiBuilder/EccsGuiBuilder/src/client/Wrappers/AutoAssign/AssignMe.cs
+++ EccsGuiBuilder/EccsGuiBuilder/src/client/Wrappers/AutoAssign/AssignMe.cs
@@ -1,7 +1,9 @@
 using System;
+using JetBrains.Annotations;
 
 namespace EccsGuiBuilder.Client.Wrappers.AutoAssign
 {
+	[MeansImplicitUse(ImplicitUseKindFlags.Assign)]
 	[AttributeUsage(AttributeTargets.Field)]
 	public class AssignMe : Attribute
 	{
